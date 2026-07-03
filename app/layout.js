155bfe213e84e93e5e8d198dfdba7c644a55d8cb
ruizmaxime51-art <ruiz.maxime51@gmail.com>
2026-07-03 10:56:56 +0200

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css';
 
 const display = Fraunces({
   subsets: ['latin'],
-  weight: ['400', '500', '560', '600', '700'],
+  weight: ['400', '500', '600', '700'],
   style: ['normal', 'italic'],
   variable: '--font-display-loaded',
   display: 'swap',
